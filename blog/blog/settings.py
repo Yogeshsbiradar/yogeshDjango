6f0a4406aba12663f2b26445475dbe7fc282c2ea
Yogeshsbiradar <39198988+Yogeshsbiradar@users.noreply.github.com>
2020-04-03 23:38:29 +0530

    Update settings.py

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_PATH=os.path.realpath(os.path.dirname(__file))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [PROJECT_PATH+'/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
